77ea6bcdfc4933dff491b499059a2e1716746a43
Dharshan <dharshansundaram28@gmail.com>
2026-07-16 18:24:14 +0530

    Check fail function

--- test_app.py
+++ test_app.py
@@ -1,8 +1,6 @@
 from app import add, multiply, subtract 
 
-def test_add():
-    assert add(5, 3) == 8
-    print("Add test passed!")
+
 
 def test_subtract():
     assert subtract(10, 3) == 7
@@ -13,7 +11,11 @@ def test_multiply():
     assert multiply(4, 3) == 12
     print("Multiply test passed!")
 
+def test_add():
+    assert add(5, 3) == 99
+
 test_add()
 test_multiply()
 test_subtract()
+
 print("All tests passed!")
